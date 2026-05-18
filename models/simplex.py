3f76203b7b4bf91233012a684c8c6f105aa83b46
VrajVed <vraj.a.ved@gmail.com>
2026-05-18 22:50:56 +0530

    final simplex schema (For now)

--- models/simplex.py
+++ models/simplex.py
@@ -1,55 +1,35 @@
-from pydantic import BaseModel, ConfigDict, StringConstraints
+from pydantic import BaseModel, StringConstraints
 from typing import Literal, Annotated
 
 
 VariableName = Annotated[str, StringConstraints(pattern=r"^[xsa][1-9][0-9]*$")]
 
-class SimplexConstraints(BaseModel):
+
+class SimplexProblem(BaseModel):
+    objective: Literal["max", "min"]
+    objective_coefficients: list[float]
+    constraints: list[SimplexConstraint]
+
+
+class SimplexConstraint(BaseModel):
     coefficients: list[float]
     operator: Literal["<=", ">=", "="]
     rhs: float
 
 
+class SimplexTableau(BaseModel):
+    iteration: int
+    matrix: list[list[float]]
+    basis: list[VariableName]
+    column_variables: list[VariableName]
 
-class SimplexProblem(BaseModel):
-    objective: Literal["max", "min"]
-    objective_coefficients: list[float]
-    constraints: list[SimplexConstraints]
+    
 
 
 class SimplexIteration(BaseModel):
-    iteration: int
-    basis: list[VariableName]
     entering_variable: VariableName
     leaving_variable: VariableName
     pivot_row: int
     pivot_column: int
+    tableau: SimplexTableau
 
-
-# problem = SimplexProblem(
-#     objective="max",
-#     objective_coefficients=[3, 2],
-#     constraints=[
-#         SimplexConstraints(
-#             coefficients=[1, 1],
-#             operator="<=",
-#             rhs=4
-#         ),
-#         SimplexConstraints(
-#             coefficients=[2, 1],
-#             operator="<=",
-#             rhs=6
-#         )
-#     ]
-# )
-
-# iteration = SimplexIteration(
-#     iteration=1,
-#     basis=[1, 2],
-#     entering_variable="x1",
-#     leaving_variable="x2",
-#     pivot_element=[0, 0]
-# )
-
-print(problem.model_dump_json(indent=2))
-print(iteration.model_dump_json(indent=2))
